8a8140e8176541679002ade8f5981bb6dddf373d
bob <bob@example.com>
2013-03-12 11:28:01 +0800

    inited repos

---
+++ pow.c
@@ -0,0 +1,16 @@
+/*#include <math.h>*/
+/*void main()*/
+/*{*/
+  /*double base, exp, result;*/
+  /*base = 5;*/
+  /*exp = 5;*/
+  /*result = pow(base,  exp);*/
+  /*printf("%d", result);*/
+/*}*/
+
+#include <stdlib.h>
+#include <math.h>
+void main()
+{
+  printf("计算4的5次方: 4^5=%f \n",pow(4.,5.));
+}
